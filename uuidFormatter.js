a911c2ec21228c3e6c10ebd57bd7314e4de9c928
Patricia Moraes <patriciamoraes@Victors-MacBook-Pro.local>
2021-01-26 19:14:08 -0800

    Adding comments

--- uuidFormatter.js
+++ uuidFormatter.js
@@ -1,13 +1,13 @@
 module.exports = function uuidFormater(decimalValue) {
   
-  // formats the hexadecimal into UUID
+  // Function that formats the hexadecimal into UUID
   const generateUUIDFormat = (hexValue) => {
       const zeroAsUUID = '00000000-0000-0000-0000-000000000000'
       // remove the appropriate number of 0's and replace them with the hexValue
       return zeroAsUUID.substring(0, zeroAsUUID.length - hexValue.length) + hexValue
   }
 
-  // converts a decimal number into hexadecimal
+  // Function that converts a decimal number into hexadecimal
   const convertToUUID = (decimalValue) => {
       const hexValue = decimalValue.toString(16)
       return generateUUIDFormat(hexValue)
--- writeFile.js
+++ writeFile.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = function writeFile(fileName, script) {
     
-      
+      // Function that writes the SQL file
       fs.writeFile(fileName, script, function (err) {
       if (err) return console.log(err);
         console.log('Writing to seed-data.sql');
